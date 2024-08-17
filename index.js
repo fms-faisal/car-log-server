398b1c2d19ce7b945f80767f9516c1f398f5066a
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-08-17 08:36:56 +0600

    initial server setup with express,cors, dotenv, mongoose, nodemon

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+app.use(express.json()) //convert json to normal object
+
+app.get('/', (req, res) => {
+    res.send("hello server")
+})
+
+app.listen(port, ()=> console.log(`server running on port ${port}`))
