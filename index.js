b94f600713960ccd6b9fa0e273fb5a493e92716c
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-08-17 12:59:27 +0600

    api to get all data implemented

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
+    const carsCollection = client.db("carlog").collection("cars");
+
+    app.get('/cars', async (req, res)=> {
+        const result = await carsCollection.find().toArray()
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
